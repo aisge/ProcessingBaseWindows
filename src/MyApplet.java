212ab8d13448e031eeb161ae1d5acde274616460
aisge <33798350+aisge@users.noreply.github.com>
2022-02-13 13:22:58 +0100

    add setup() and keyPressed()-Method

--- src/MyApplet.java
+++ src/MyApplet.java
@@ -2,14 +2,26 @@ import processing.core.PApplet;
 
 public class MyApplet extends PApplet {
 
+    @Override
     public void settings() {
         size(640, 480);
     }
 
+    @Override
+    public void setup() {
+        frameRate(30);
+    }
+
+    @Override
     public void draw() {
         background(0);
         fill(0, 255,0);
         text("Implement me...", 50, 50);
+    }
+
+    @Override
+    public void keyPressed() {
+
     }
 
 }
