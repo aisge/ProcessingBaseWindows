f68034fc71512a2411fad943b16f9974aa2f3c3f
aisge <33798350+aisge@users.noreply.github.com>
2022-02-13 13:12:35 +0100

    first commit

---
+++ src/MyApplet.java
@@ -0,0 +1,15 @@
+import processing.core.PApplet;
+
+public class MyApplet extends PApplet {
+
+    public void settings() {
+        size(640, 480);
+    }
+
+    public void draw() {
+        background(0);
+        fill(0, 255,0);
+        text("Implement me...", 50, 50);
+    }
+
+}
---
+++ src/TheMain.java
@@ -0,0 +1,7 @@
+import processing.core.PApplet;
+
+public class TheMain {
+    public static void main(String[] args) {
+        PApplet.main("MyApplet");
+    }
+}
